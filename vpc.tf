4a8687c252b047240d3e5f51da9a2347a123a814
ppabis <piotr@pabis.eu>
2023-07-29 16:52:00 +0200

    formatting

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "My-VPC" {
-  cidr_block = "10.100.0.0/16"
+  cidr_block                       = "10.100.0.0/16"
   assign_generated_ipv6_cidr_block = true
-  enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
   tags = {
     Name = "My-VPC"
   }
